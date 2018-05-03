b04ce73174712638fff55b43c8f837a4c6089c4b
Georgi Yankov <gyankov@progress.com>
2018-05-03 11:24:43 +0300

    chore: remove unused dependencies

--- TripXpert/Startup.cs
+++ TripXpert/Startup.cs
@@ -1,14 +1,10 @@
-using Microsoft.Owin;
-using Owin;
 
-[assembly: OwinStartupAttribute(typeof(TripXpert.Startup))]
 namespace TripXpert
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
+        public void Configuration()
         {
-            ConfigureAuth(app);
         }
     }
 }
