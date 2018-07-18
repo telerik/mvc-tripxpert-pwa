77411f6673b0d7be8d21ea09bdc81ed83c47bfbe
Georgi Yankov <gyankov@progress.com>
2018-07-18 17:27:52 +0300

    chore: exclude google analitycs from cache

--- TripXpert/service-worker.js
+++ TripXpert/service-worker.js
@@ -14,11 +14,21 @@ self.addEventListener('activate', function (e) {
 });
 
 self.addEventListener('fetch', function (e) {
+    if (e.request.url.indexOf("google") !== -1 ||
+        e.request.url.indexOf("demandbase") !== -1 ||
+        e.request.url.indexOf("en25") !== -1 ||
+        e.request.url.indexOf("d.company") !== -1 ||
+        e.request.url.indexOf("d.rlcdn") !== -1 ||
+        e.request.url.indexOf("t.eloqua") !== -1
+    ) {
+        return
+    }
+
     e.respondWith(
         caches.match(e.request.url).then(function (resp) {
             return resp || fetch(e.request.url).then(function (response) {
                 var clonedResponse = response.clone();
-                
+
                 caches.open(cacheName).then(function (cache) {
                     cache.put(e.request.url, clonedResponse);
                 });
