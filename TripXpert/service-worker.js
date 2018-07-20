8fa6c20963be6df7be87e23911f5fb17d6c37b05
Georgi Yankov <gyankov@progress.com>
2018-07-20 16:24:42 +0300

    chore: cache only relevant data

--- TripXpert/service-worker.js
+++ TripXpert/service-worker.js
@@ -14,19 +14,14 @@ self.addEventListener('activate', function (e) {
 });
 
 self.addEventListener('fetch', function (e) {
-    if (e.request.url.indexOf("google") !== -1 ||
-        e.request.url.indexOf("demandbase") !== -1 ||
-        e.request.url.indexOf("en25") !== -1 ||
-        e.request.url.indexOf("d.company") !== -1 ||
-        e.request.url.indexOf("d.rlcdn") !== -1 ||
-        e.request.url.indexOf("t.eloqua") !== -1
+    if (e.request.url.indexOf("kendo") === -1 ||
+        e.request.url.indexOf("tripxpert") === -1 ||
     ) {
         return;
     }
 
     e.respondWith(
         caches.match(e.request.url).then(function (resp) {
-            e.request.redirect = "follow";
             return resp || fetch(e.request.url).then(function (response) {
                 var clonedResponse = response.clone();
 
