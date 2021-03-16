3c92bb6ab7930057bc2e29b73613b12924eb7ce7
Mihail Parvanov <mihail.parvanov@progress.com>
2021-03-16 11:22:08 +0200

    chore: Update version script

--- update-version.sh
+++ update-version.sh
@@ -7,8 +7,8 @@ echo "Release version: curl -s https://api.github.com/repos/telerik/kendo-ui-cor
 
 
 
-
-
+CURRENT_VERSION="2021.1.119"
 LAST_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4)
 echo $LAST_RELEASE
 
+sed -i "s/$CURRENT_VERSION/$LAST_RELEASE/g" TripXpert/Views/Shared/_Layout.cshtml
