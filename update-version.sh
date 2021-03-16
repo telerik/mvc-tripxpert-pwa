b63681f7e301a4d406fb7269145d0a7758d08dae
Mihail Parvanov <mihail.parvanov@progress.com>
2021-03-16 16:34:05 +0200

    Update version with RegEx

--- update-version.sh
+++ update-version.sh
@@ -1,8 +1,8 @@
 echo "NPM version: npm show @progress/kendo-ui version"
-npm show @progress/kendo-ui version
+#npm show @progress/kendo-ui version
 #curl  https://api.github.com/repos/telerik/kendo-ui-core/releases
 #echo $("curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases")
-echo "Release version: curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '\"' -f 4"
+#echo "Release version: curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '\"' -f 4"
 #curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4
 
 
@@ -13,4 +13,7 @@ echo $CURRENT_VERSION
 LAST_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4)
 echo $LAST_RELEASE
 
-sed -i "s/$CURRENT_VERSION/$LAST_RELEASE/g" TripXpert/Views/Shared/_Layout.cshtml
+#sed -i "s/$CURRENT_VERSION/$LAST_RELEASE/g" TripXpert/Views/Shared/_Layout.cshtml
+
+sed -i "s/kendo.cdn.telerik.com\/[^\/]*/kendo.cdn.telerik.com\/$LAST_RELEASE/g" TripXpert/Views/Shared/_Layout.cshtml
+
