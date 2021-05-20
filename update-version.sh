0a3c5382718a99cb164f216a120e4f4947459da8
Mihail Parvanov <mihail.parvanov@progress.com>
2021-05-20 14:37:59 +0300

    feat: Add array for files with CDN to be looped telerik/kendo#10558 telerik/kendo#12283

--- update-version.sh
+++ update-version.sh
@@ -1,19 +1,10 @@
 echo "NPM version: npm show @progress/kendo-ui version"
-#npm show @progress/kendo-ui version
-#curl  https://api.github.com/repos/telerik/kendo-ui-core/releases
-#echo $("curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases")
-#echo "Release version: curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '\"' -f 4"
-#curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4
-
-
-
-#CURRENT_VERSION="2021.1.119"
-CURRENT_VERSION=$(grep -hnr "kendo.cdn" TripXpert/Views/Shared/_Layout.cshtml | head -1 |cut -d '/' -f 4)
-echo $CURRENT_VERSION
 LAST_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4)
 echo $LAST_RELEASE
-
-#sed -i "s/$CURRENT_VERSION/$LAST_RELEASE/g" TripXpert/Views/Shared/_Layout.cshtml
-
-sed -i "s/kendo.cdn.telerik.com\/[^\/]*/kendo.cdn.telerik.com\/$LAST_RELEASE/g" TripXpert/Views/Shared/_Layout.cshtml
-
+files=(TripXpert/TripXpert.csproj TripXpert/Views/Contact/Contact.cshtml TripXpert/Views/Destinations/DestinationDetails.cshtml TripXpert/Views/Destinations/Destinations.cshtml TripXpert/Views/Home/Index.cshtml TripXpert/Views/Shared/_Layout.cshtml TripXpert/packages.config TripXpert/service-worker.js)
+for file in ${files[@]}
+do
+CURRENT_VERSION=$(grep -hnr "kendo.cdn" $file | head -1 |cut -d '/' -f 4)
+echo "$file $CURRENT_VERSION"
+sed -i "s/kendo.cdn.telerik.com\/[^\/]*/kendo.cdn.telerik.com\/$LAST_RELEASE/g" $file
+done
