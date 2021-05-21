64bab0092b2ca966eec5a14a03e2eccd105691ab
Mihail Parvanov <mihail.parvanov@progress.com>
2021-05-21 08:43:36 +0300

    chore: Replace current version with latest in all listed files telerik/kendo#10558 telerik/kendo#12283

--- update-version.sh
+++ update-version.sh
@@ -1,10 +1,12 @@
-echo "NPM version: npm show @progress/kendo-ui version"
+layoutFile="TripXpert/Views/Shared/_Layout.cshtml";
+CURRENT_VERSION=$(grep -hnr "kendo.cdn" $layoutFile | head -1 |cut -d '/' -f 4)
+echo "Current release version from $layoutFile is $CURRENT_VERSION"
+
 LAST_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4)
-echo $LAST_RELEASE
+echo "Last release version is $LAST_RELEASE"
+
 files=(TripXpert/TripXpert.csproj TripXpert/Views/Contact/Contact.cshtml TripXpert/Views/Destinations/DestinationDetails.cshtml TripXpert/Views/Destinations/Destinations.cshtml TripXpert/Views/Home/Index.cshtml TripXpert/Views/Shared/_Layout.cshtml TripXpert/packages.config TripXpert/service-worker.js)
 for file in ${files[@]}
 do
-CURRENT_VERSION=$(grep -hnr "kendo.cdn" $file | head -1 |cut -d '/' -f 4)
-echo "$file $CURRENT_VERSION"
-sed -i "s/kendo.cdn.telerik.com\/[^\/]*/kendo.cdn.telerik.com\/$LAST_RELEASE/g" $file
+sed -i "s/$CURRENT_VERSION/$LAST_RELEASE/g" $file
 done
