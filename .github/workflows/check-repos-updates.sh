c7be704418bb9b31066cf14e78aea67bbc04a9ee
Viktor Tachev <vtachev@progress.com>
2025-12-02 17:35:27 +0200

    fix: get correct themes version

--- .github/workflows/check-repos-updates.sh
+++ .github/workflows/check-repos-updates.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "Stage1 Find Updates"
 LATEST_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-ui-core/releases | grep tag_name | head -n 1 |  cut -d '"' -f 4)
-LATEST_THEMES_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-themes/releases | grep -B4 '"prerelease": false' | head -1 | cut -d '"' -f 4 | tr -d v)
+LATEST_THEMES_RELEASE=$(curl -s https://api.github.com/repos/telerik/kendo-themes/releases | jq -r '[.[] | select(.prerelease == false)][0].name' | tr -d v)
 echo "Last release version is $LATEST_RELEASE"
 echo "Last Themes release version is $LATEST_THEMES_RELEASE"
 
 function getCurrentVersion {
-    for file in `find . -type f -name "_Layout.cshtml"`  
+    for file in `find . -type f -name "_Layout.cshtml"`
     do
         CURRENT_VERSION=$(grep -hnr "kendo.cdn" $file | head -2 | tail -1 | cut -d '/' -f 4)
         if [ ! -z "$CURRENT_VERSION" ]
@@ -17,7 +17,7 @@ function getCurrentVersion {
 }
 
 function getCurrentThemesVersion {
-    for file in `find . -type f -name "_Layout.cshtml"`  
+    for file in `find . -type f -name "_Layout.cshtml"`
     do
         CURRENT_THEMES_VERSION=$(grep -hnr "kendo.cdn" $file | head -1 | cut -d '/' -f 5)
         if [ ! -z "$CURRENT_THEMES_VERSION" ]
@@ -33,16 +33,16 @@ function getCurrentThemesVersion {
     echo "Current themes version is $CURRENT_GLOBAL_THEMES_VERSION"
 
 
-for file in `find . -type f -name "*.cshtml"`  
+for file in `find . -type f -name "*.cshtml"`
 do
     sed -i "s/$CURRENT_GLOBAL_VERSION/$LATEST_RELEASE/g" $file
     sed -i "s/$CURRENT_GLOBAL_THEMES_VERSION/$LATEST_THEMES_RELEASE/g" $file
 done
-for file in `find . -type f -name "*.csproj"`  
+for file in `find . -type f -name "*.csproj"`
 do
     sed -i "s/$CURRENT_GLOBAL_VERSION/$LATEST_RELEASE/g" $file
 done
-for file in `find . -type f -name "*.config"`  
+for file in `find . -type f -name "*.config"`
 do
     sed -i "s/$CURRENT_GLOBAL_VERSION/$LATEST_RELEASE/g" $file
 done
